c6a0a4ca3083eacca829e559b3fdbb72dcce94ba
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-08-21 23:17:44 +0900

    Update Baekjoon1964.java
    Algorithm error, update clear

--- Baekjoon/Baekjoon1964.java
+++ Baekjoon/Baekjoon1964.java
@@ -15,17 +15,13 @@ public class Baekjoon1964 {
 		int n = scan.nextInt(); // 몇 단계를 증가하는지 알기 위한 입력값 선언
 		int result = 5; // 초기값이자 결과값을 저장하기 위한 변수 설정
 		
-		
 		// 알고리즘
-		if(n == 1) { // 1단계일 경우
-			System.out.println(result); // 초기값 그대로 결과 출력
-		} else {
-			for(int i = 1; i < n; i++) { // 1단계가 아닐 경우
-				// 결과값 = 초기값 + (각 단계 + 증가한 겹치지 않는 점) * 변의 개수 - 겹치는 점의 개수
-				result = result + (i + 2) * 3 - 2;
-			}
-		}
-		System.out.println(result % 45678); // 문제 조건에 맞는 계산 후 결과 출력
+		
+		for(int i = 1; i < n; i++) { 
+			// 결과값 = (초기값 + (각 단계 + 증가한 겹치지 않는 점) * 변의 개수 - 겹치는 점의 개수)
+		    result = (result + (i + 2) * 3 - 2);
+	    }
+	    
+	    System.out.println(result % 45678); // 문제 조건에 맞는 계산 후 결과 출력
 	}
-
 }
