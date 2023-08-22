2768a49e9d49af7f927eb330b32e2cb44cc69030
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-08-22 22:30:55 +0900

    Create Baekjoon1008.java
    Baekjoon 1008 clear

---
+++ Baekjoon/Baekjoon1008.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        
+        System.out.println(a / b);
+        
+    }
+} 
