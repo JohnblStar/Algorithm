527abc1bbb6ced519dabb6f2874b953f6418cfe8
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-08-22 22:39:52 +0900

    Create Baekjoon18108.java
    Baekjoon18108 clear

---
+++ Baekjoon/Baekjoon18108.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        int s = scan.nextInt();
+        
+        System.out.println(s - 543);
+        
+    }
+}
