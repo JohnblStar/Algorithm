ea2d1c8f76b37189b8b40ef701632345b1aef172
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-08-22 23:55:52 +0900

    Create Baekjoon11382.java

---
+++ Baekjoon/Level1/Baekjoon11382.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+       Scanner scan = new Scanner(System.in);
+
+      //타입을 롱으로 받아와야함
+       long a = scan.nextLong();
+       long b = scan.nextLong();
+       long c = scan.nextLong();
+       
+       System.out.println(a + b + c);
+    }
+}
