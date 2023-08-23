f00be575f4eb769c67dc7b8327974af109fec020
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-08-23 22:31:40 +0900

    Create Baekjoon10171.java

---
+++ Baekjoon/Level1/Baekjoon10171.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+       
+       //Escape Sequance 주의
+       
+       System.out.println("\\    /\\");
+       System.out.println(" )  ( ')");
+       System.out.println("(  /  )");
+       System.out.println(" \\(__)|");
+    }
+}
