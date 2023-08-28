acd33a122ec870d1b5add665ac25bc2c738a0ec5
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-08-28 22:22:54 +0900

    Create Baekjoon10950.java

---
+++ Baekjoon/StepByStep/Level3/Baekjoon10950.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        int T = scan.nextInt();
+        
+        for(int i = 0; i < T; i++){
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            
+            System.out.println(a + b);
+        }
+    }
+}
