fccd0cdc05f2e4e8a7f876b5497fd607cc5ed3f4
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-09-01 23:09:32 +0900

    Create Baekjoon2743.java

---
+++ Baekjoon/StepByStep/Level4/Baekjoon2743.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        bw.write(String.valueOf(str.length()));
+        bw.flush();
+        bw.close();
+    }
+}
