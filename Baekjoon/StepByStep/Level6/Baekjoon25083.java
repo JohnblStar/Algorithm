471f786de84e46573ab56cb3ca49fb19a883205b
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-09-13 22:11:35 +0900

    Create Baekjoon25083.java

---
+++ Baekjoon/StepByStep/Level6/Baekjoon25083.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("         ,r'\"7" + "\n");
+        bw.write("r`-_   ,'  ,/" + "\n");
+        bw.write(" \\. \". L_r'" + "\n");
+        bw.write("   `~\\/" + "\n");
+        bw.write("      |" + "\n");
+        bw.write("      |");
+        bw.close();
+    }
+}
