89d1a3e4cae9dd40dc118460871cd4338cb9ac05
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-09-27 22:18:49 +0900

    Update Baekjoon10988.java - Update logic, success.

--- Baekjoon/StepByStep/Level6/Baekjoon10988.java
+++ Baekjoon/StepByStep/Level6/Baekjoon10988.java
@@ -5,22 +5,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str = br.readLine();
-        char[] strArr = new char[str.length()];
-        int max = str.length() / 2;
-        int min = 1;
-        int result = 0;
+        int result = 1;
         
-        for(int i = 0; i < str.length(); i++) {
-            strArr[i] = str.charAt(i);
-        }
-        
-        for(int i = 0; i < max; i++) {
-            if(strArr[i] == strArr[strArr.length - min]) {
-                result = 1;
-                min++;
-            } else {
+        for(int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            
+            if(ch != str.charAt(str.length() - i - 1)) {
                 result = 0;
-                break;
             }
         }
         
