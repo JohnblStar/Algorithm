8a431b7fcf05fed866e80b8025779db805931fd4
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-10-04 23:31:04 +0900

    Update Baekjoon2745.java - not to success,change mind.

--- Baekjoon/StepByStep/Level8/Baekjoon2745.java
+++ Baekjoon/StepByStep/Level8/Baekjoon2745.java
@@ -11,6 +11,7 @@ public class Main {
         int B = Integer.valueOf(strtk.nextToken());
         char[] arr36 = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V' ,'W' ,'X' , 'Y' ,'Z'};
         int val = 0;
+        int result = 0;
         
         
         for(int i = 0; i < N.length(); i++) {
@@ -18,16 +19,17 @@ public class Main {
             for(int j = 0; j < B; j++) {
                 if(i != 0) {
                     if(ch == arr36[j]){
-                        val += Math.pow(36 , i) * j ;
+                        val = Math.pow(36 , i) * j ;
                     }
                 } else if(i == 0){
                     if(ch == arr36[j]){
-                        val += j;
+                        val = j;
                     }
                 }
+                result += val;
             }
         }
-        bw.write(String.valueOf(val));
+        bw.write(String.valueOf(result));
         bw.close();
     }
 }
