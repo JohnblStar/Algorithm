28ce2c5f861bffd15218c5e9302faf2210eb2ee3
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-10-10 20:47:42 +0900

    Update Baekjoon2557.java

--- Baekjoon/StepByStep/Level1/Baekjoon2557.java
+++ Baekjoon/StepByStep/Level1/Baekjoon2557.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        //String str = "Hello World!";
+        //System.out.println(str);
     }
 }
