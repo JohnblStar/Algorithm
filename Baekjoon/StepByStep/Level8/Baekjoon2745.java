57b841b9ffbe8f54901fee363824a80b7e7dbbab
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-10-11 23:28:14 +0900

    Update Baekjoon2745.java - success. clear code.

--- Baekjoon/StepByStep/Level8/Baekjoon2745.java
+++ Baekjoon/StepByStep/Level8/Baekjoon2745.java
@@ -9,27 +9,22 @@ public class Main {
         StringTokenizer strtk = new StringTokenizer(br.readLine());
         String N = strtk.nextToken();
         int B = Integer.valueOf(strtk.nextToken());
-        char[] arr36 = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V' ,'W' ,'X' , 'Y' ,'Z'};
-        int val = 0;
         int result = 0;
+        int tmp = 1;
         
         
-        for(int i = 0; i < N.length(); i++) {
+        for(int i = N.length()-1; i>=0; i--){
             char ch = N.charAt(i);
-            for(int j = 0; j < B; j++) {
-                if(i != 0) {
-                    if(ch == arr36[j]){
-                        val = Math.pow(36 , i) * j ;
-                    }
-                } else if(i == 0){
-                    if(ch == arr36[j]){
-                        val = j;
-                    }
-                }
-                result += val;
+            if('A' <= ch && ch <= 'Z'){
+                result += (ch - 'A'+10) * tmp;
             }
+            else{
+                result += (ch - '0') * tmp;
+            }
+            tmp *= B;
         }
-        bw.write(String.valueOf(val));
+
+        bw.write(String.valueOf(result));
         bw.close();
     }
 }
