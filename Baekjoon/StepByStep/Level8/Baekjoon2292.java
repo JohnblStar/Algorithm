0a5356cb195c4130b4559af966233e2e6b881ed0
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-10-19 22:40:26 +0900

    Update Baekjoon2292.java - success.

--- Baekjoon/StepByStep/Level8/Baekjoon2292.java
+++ Baekjoon/StepByStep/Level8/Baekjoon2292.java
@@ -1,19 +1,28 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int[] arr = {1, 7, 19, 37, 61, 70};
+        ArrayList<Integer> list = new ArrayList<>();
         int N = Integer.valueOf(br.readLine());
+        int a = 1;
+        int six = 6;
         int cnt = 1;
+        
+        while(a <= N){
+            list.add(a);
+            a += six;
+            six += 6;
+        }
         
         if(N == 1) {
             cnt = 1;
         } else {
-            for(int i = 0; i < 6; i++) {
-             if(N > arr[i]) {
+            for(int i = 0; i < list.size(); i++) {
+             if(N > list.get(i)) {
                     cnt++;
                 } 
             }
