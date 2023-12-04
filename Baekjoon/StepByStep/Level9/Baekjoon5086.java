818e8e42b8d3b94aedc801c02ac99438b5dea08a
HanByeol-Jo <84443670+JohnblStar@users.noreply.github.com>
2023-12-04 22:53:00 +0900

    Create Baekjoon5086.java

---
+++ Baekjoon/StepByStep/Level9/Baekjoon5086.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWirter(new OutputStreamWrier(System.out));
+        StringTokenizer strtk = new StringTokenizer(br.readLine());
+    
+        int a = 0;
+        int b = 0;
+        
+        while(true){
+            a = Integer.valueOf(strtk.nextToken());
+            b = Integer.valueOf(strtk.nextToken());
+            if(a == 0 && b == 0){
+                break;
+            } else {
+                
+            }
+        }
+    }
+}
